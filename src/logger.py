dc316087b2cf5a42c033d9c1bc902ed33c7c47ed
GermanMT <germanoctako@gmail.com>
2025-10-08 17:37:53 +0200

    feat: add logger manager

---
+++ src/logger.py
@@ -0,0 +1,48 @@
+from functools import lru_cache
+from logging import INFO, Formatter, getLogger
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+
+class LoggerManager:
+    def __init__(
+        self,
+        log_file: str = "errors.log",
+        max_bytes: int = 5 * 1024 * 1024,
+        backup_count: int = 5,
+    ):
+        self.logger = getLogger("securechain")
+        self.logger.setLevel(INFO)
+        self.logger.propagate = False
+
+        log_path = Path(log_file)
+        log_path.parent.mkdir(parents=True, exist_ok=True)
+
+        file_handler = RotatingFileHandler(
+            log_file, maxBytes=max_bytes, backupCount=backup_count, encoding="utf-8"
+        )
+        formatter = Formatter(
+            "%(asctime)s - %(levelname)s - %(name)s - %(filename)s:%(lineno)d - %(message)s"
+        )
+        file_handler.setFormatter(formatter)
+
+        if not self.logger.handlers:
+            self.logger.addHandler(file_handler)
+
+    def info(self, msg: str, *args, **kwargs):
+        self.logger.info(msg, *args, **kwargs)
+
+    def warning(self, msg: str, *args, **kwargs):
+        self.logger.warning(msg, *args, **kwargs)
+
+    def error(self, msg: str, *args, **kwargs):
+        self.logger.error(msg, *args, **kwargs)
+
+    def exception(self, msg: str, *args, **kwargs):
+        self.logger.exception(msg, *args, **kwargs)
+
+@lru_cache
+def get_logger() -> LoggerManager:
+    return LoggerManager("logs/errors.log")
+
+logger: LoggerManager = get_logger()
