1432c36f56c6dd61c50e2da7e643bbc559edad40
GermanMT <amtrujillo@us.es>
2025-11-24 16:24:40 +0100

    chore: add database settings

--- src/settings.py
+++ src/settings.py
@@ -20,6 +20,15 @@ class Settings(BaseSettings):
     REDIS_GROUP: str = "extractors"
     REDIS_CONSUMER: str = "package-consumer"
 
+    # Database Configuration
+    DB_MIN_POOL_SIZE: int = 10
+    DB_MAX_POOL_SIZE: int = 100
+    DB_MAX_IDLE_TIME_MS: int = 60000
+    DB_DEFAULT_QUERY_TIMEOUT_MS: int = 30000
+    DB_SMTS_COLLECTION: str = "smts"
+    DB_OPERATION_RESULT_COLLECTION: str = "operation_results"
+    DB_API_KEY_COLLECTION: str = "api_keys"
+    DB_VULNERABILITIES_COLLECTION: str = "vulnerabilities"
 
 @lru_cache
 def get_settings() -> Settings:
