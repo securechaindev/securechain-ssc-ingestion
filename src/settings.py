c2bc609e920fbe4557c74120bee6075bde1cdb6a
GermanMT <amtrujillo@us.es>
2025-12-01 15:10:39 +0100

    chore: add settings class default values with pydantic field

--- src/settings.py
+++ src/settings.py
@@ -1,24 +1,25 @@
 from functools import lru_cache
 
-from pydantic import ConfigDict
+from pydantic import ConfigDict, Field
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     model_config = ConfigDict(env_file=".env")
 
-    GRAPH_DB_URI: str = "bolt://localhost:7687"
-    GRAPH_DB_USER: str = "neo4j"
-    GRAPH_DB_PASSWORD: str = "neoSecureChain"
-    VULN_DB_URI: str = "mongodb://mongoSecureChain:mongoSecureChain@localhost:27017/admin"
-    VULN_DB_USER: str = "mongoSecureChain"
-    VULN_DB_PASSWORD: str = "mongoSecureChain"
-    REDIS_HOST: str = "localhost"
-    REDIS_PORT: int = 6379
-    REDIS_DB: int = 0
-    REDIS_STREAM: str = "package_extraction"
-    REDIS_GROUP: str = "extractors"
-    REDIS_CONSUMER: str = "package-consumer"
+    # Database connections (required)
+    GRAPH_DB_URI: str = Field(..., alias="GRAPH_DB_URI")
+    GRAPH_DB_USER: str = Field(..., alias="GRAPH_DB_USER")
+    GRAPH_DB_PASSWORD: str = Field(..., alias="GRAPH_DB_PASSWORD")
+    VULN_DB_URI: str = Field(..., alias="VULN_DB_URI")
+
+    # Redis Configuration
+    REDIS_HOST: str = Field("localhost", alias="REDIS_HOST")
+    REDIS_PORT: int = Field(6379, alias="REDIS_PORT")
+    REDIS_DB: int = Field(0, alias="REDIS_DB")
+    REDIS_STREAM: str = Field("package-extraction", alias="REDIS_STREAM")
+    REDIS_GROUP: str = Field("extractors", alias="REDIS_GROUP")
+    REDIS_CONSUMER: str = Field("package-consumer", alias="REDIS_CONSUMER")
 
     # Database Configuration
     DB_MIN_POOL_SIZE: int = 10
@@ -29,6 +30,7 @@ class Settings(BaseSettings):
     DB_OPERATION_RESULT_COLLECTION: str = "operation_results"
     DB_API_KEY_COLLECTION: str = "api_keys"
     DB_VULNERABILITIES_COLLECTION: str = "vulnerabilities"
+
 
 @lru_cache
 def get_settings() -> Settings:
